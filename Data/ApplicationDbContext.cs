8a80f8451bcc989f9ac41d90840a57d2fc9547d9
Sai Ram T <sairam751348@gmail.com>
2023-10-09 13:33:35 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Bookstore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "FeelGood", DisplayOrder = 3}
+                );
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Category
+    {
+
+        //If name of the primary key is ID only then we dont require Data Annotation to set the primary key or else even if name is CategoryId then also we dont reuire data annotations
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
